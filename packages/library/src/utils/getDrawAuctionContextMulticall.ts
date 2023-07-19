4986ccdcf58f7fbafaf88cc0bf8fd205253870d3
Charles Bergeron <chuck.bergeron@gmail.com>
2023-07-19 13:44:59 -0700

    update code to follow latest changes to contracts

--- packages/library/src/utils/getDrawAuctionContextMulticall.ts
+++ packages/library/src/utils/getDrawAuctionContextMulticall.ts
@@ -12,8 +12,10 @@ const { MulticallWrapper } = ethersMulticallProviderPkg;
 const REWARD_DECIMALS_KEY = 'reward-decimals';
 const REWARD_NAME_KEY = 'reward-name';
 const REWARD_SYMBOL_KEY = 'reward-symbol';
-const RNG_IS_AUCTION_OPEN_KEY = 'rng-isAuctionOpen';
+const RNG_IS_AUCTION_COMPLETE = 'rng-isAuctionOpen';
 const RNG_CURRENT_REWARD_PORTION_KEY = 'rng-currentRewardPortion';
+const DRAW_IS_AUCTION_COMPLETE = 'draw-isAuctionOpen';
+const DRAW_CURRENT_REWARD_PORTION_KEY = 'draw-currentRewardPortion';
 const PRICE_FEED_PREFIX_KEY = 'priceFeed';
 
 /**
@@ -40,33 +42,29 @@ export const getDrawAuctionContextMulticall = async (
   queries[REWARD_NAME_KEY] = rewardTokenContract.name();
   queries[REWARD_SYMBOL_KEY] = rewardTokenContract.symbol();
 
-  // 2. Native token (gas token) market rate in USD
-  const gasTokenMarketRateUsd = await getGasTokenMarketRateUsd(auctionContracts.marketRateContract);
-
-  // 3. RNG Auction info
-  queries[RNG_IS_AUCTION_OPEN_KEY] = auctionContracts.rngAuctionContract.isRNGAuctionOpen();
-
-  queries[RNG_CURRENT_REWARD_PORTION_KEY] =
-    auctionContracts.rngAuctionContract.currentRewardPortion();
-
-  // const currentRewardPortionDraw =
-  //   await auctionContracts.drawAuctionContract.currentRewardPortion();
-
-  // 4. Draw Auction info
-
-  // 5. Price Feed
   queries[`${PRICE_FEED_PREFIX_KEY}-${rewardTokenAddress}`] =
     auctionContracts.marketRateContract.priceFeed(rewardTokenAddress, 'USD');
 
-  // 7. Get and process results!
+  // 2. Native token (gas token) market rate in USD
+  const gasTokenMarketRateUsd = await getGasTokenMarketRateUsd(auctionContracts.marketRateContract);
+
+  // // 3. Auction info
+  // 3a. RNG Auction
+  queries[RNG_IS_AUCTION_COMPLETE] = auctionContracts.rngAuctionContract.isAuctionComplete();
+  queries[RNG_CURRENT_REWARD_PORTION_KEY] =
+    auctionContracts.rngAuctionContract.currentRewardPortion();
+
+  // 3b. Draw Auction
+  queries[DRAW_IS_AUCTION_COMPLETE] = auctionContracts.drawAuctionContract.isAuctionComplete();
+  queries[DRAW_CURRENT_REWARD_PORTION_KEY] =
+    auctionContracts.drawAuctionContract.currentRewardPortion();
+
+  //
+  // 5. Get and process results
   const results = await getEthersMulticallProviderResults(multicallProvider, queries);
 
-  const isRngAuctionOpen = results[RNG_IS_AUCTION_OPEN_KEY];
-  const currentRewardPortionRng = results[RNG_CURRENT_REWARD_PORTION_KEY];
-
+  // 5a. Results: Reward Token
   const rewardTokenPriceFeedResults = results[`${PRICE_FEED_PREFIX_KEY}-${rewardTokenAddress}`];
-
-  // 1. Reward Token results
   const rewardToken: TokenWithRate = {
     address: rewardTokenAddress,
     decimals: results[REWARD_DECIMALS_KEY],
@@ -75,10 +73,19 @@ export const getDrawAuctionContextMulticall = async (
     assetRateUsd: rewardTokenPriceFeedResults,
   };
 
+  // 5b. Results: Auction Info
+  const rngIsAuctionComplete = results[RNG_IS_AUCTION_COMPLETE];
+  const rngCurrentRewardPortion = results[RNG_CURRENT_REWARD_PORTION_KEY];
+
+  const drawIsAuctionComplete = results[DRAW_IS_AUCTION_COMPLETE];
+  const drawCurrentRewardPortion = results[DRAW_CURRENT_REWARD_PORTION_KEY];
+
   return {
-    gasTokenMarketRateUsd,
-    isRngAuctionOpen,
-    currentRewardPortionRng,
     rewardToken,
+    gasTokenMarketRateUsd,
+    rngIsAuctionComplete,
+    rngCurrentRewardPortion,
+    drawIsAuctionComplete,
+    drawCurrentRewardPortion,
   };
 };
