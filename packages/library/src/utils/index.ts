3c6f265d6ee9832e8a95a3d5dcc898ce33d89ad9
Charles Bergeron <chuck.bergeron@gmail.com>
2023-08-28 13:55:01 -0700

    greatly speed up the liquidation bot by using multicall for all the new callStatic.computeExactAmountIn calls

--- packages/library/src/utils/index.ts
+++ packages/library/src/utils/index.ts
@@ -2,6 +2,7 @@ export * from './math';
 export * from './getUsd';
 export * from './getArbLiquidatorContextMulticall';
 export * from './getLiquidationPairsMulticall';
+export * from './getLiquidationPairComputeExactAmountInMulticall';
 export * from './questions';
 export * from './logging';
 export * from './network';
