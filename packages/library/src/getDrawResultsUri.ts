abcee51f3b35eeb65e997a2a37bdc5f0863dd10f
Charles Bergeron <chuck.bergeron@gmail.com>
2023-10-20 19:55:47 -0700

    update draw results URL to new repo

--- packages/library/src/getDrawResultsUri.ts
+++ packages/library/src/getDrawResultsUri.ts
@@ -1,5 +1,5 @@
 const DRAW_RESULTS_PROTOCOL_AND_HOSTNAME = `https://raw.githubusercontent.com`;
-const DRAW_RESULTS_ORG_AND_REPO = `GenerationSoftware/pt-v5-draw-results`;
+const DRAW_RESULTS_ORG_AND_REPO = `GenerationSoftware/pt-v5-draw-results-mainnet`;
 const DRAW_RESULTS_TESTNET_ORG_AND_REPO = `GenerationSoftware/pt-v5-draw-results-testnet`;
 const DRAW_RESULTS_URI = {
   10: `${DRAW_RESULTS_PROTOCOL_AND_HOSTNAME}/${DRAW_RESULTS_ORG_AND_REPO}`,
