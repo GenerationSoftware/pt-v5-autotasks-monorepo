d024657ed8fa7536932765b254a59d62ad54417c
Charles Bergeron <chuck.bergeron@gmail.com>
2024-06-10 09:25:28 -0700

    fixes 'intrinsic gas too low' error for Arbitrum Sepolia transactions

--- packages/library/src/helpers/sendPopulatedTx.ts
+++ packages/library/src/helpers/sendPopulatedTx.ts
@@ -3,7 +3,19 @@ import { Provider } from '@ethersproject/providers';
 
 import { SendTransactionArgs } from '../types.js';
 import { printSpacer } from '../utils/index.js';
+import { CHAIN_IDS } from '../constants/network.js';
 
+/**
+ * Proxy function for anywhere we send a transaction in this monorepo
+ * - Handles proper arg passing when there is a `value` field on the SendTransactionArgs object
+ * - Fixes the gas limit for Arbitrum Sepolia
+ * - Figures out what the gasPrice should be submitted as
+ *
+ * @param {number} gasLimit, the original gasLimit, likely from provider.getEstimatedGasLimit()
+ * @param {Provider} provider, ethers.js Provider instance
+ *
+ * @returns {Promise<number>} the new gasLimit
+ */
 export const sendPopulatedTx = async (
   provider: Provider,
   wallet: Wallet,
@@ -13,6 +25,7 @@ export const sendPopulatedTx = async (
 ): Promise<ethers.providers.TransactionResponse> => {
   printSpacer();
 
+  gasLimit = await getFixedGasLimit(gasLimit, provider);
   const gasPrice = await provider.getGasPrice();
 
   const sendTransactionArgs: SendTransactionArgs = {
@@ -26,7 +39,24 @@ export const sendPopulatedTx = async (
     sendTransactionArgs.value = txParams.value;
   }
   const tx = await wallet.sendTransaction(sendTransactionArgs);
-  printSpacer();
 
   return tx;
+};
+
+/**
+ * Provides a fixed, more accurate gasLimit
+ * Arbitrum Sepolia has an 'intrinsic gas too low' issue, however if we up the gas limit it works fine
+ *
+ * @param {number} gasLimit, the original gasLimit, likely from provider.getEstimatedGasLimit()
+ * @param {Provider} provider, ethers.js Provider instance
+ *
+ * @returns {Promise<number>} the new gasLimit
+ */
+const getFixedGasLimit = async (gasLimit: number, provider: Provider): Promise<number> => {
+  // @ts-ignore
+  if (CHAIN_IDS.arbitrumSepolia === provider._network.chainId) {
+    gasLimit = gasLimit * 2;
+  }
+
+  return gasLimit;
 };
